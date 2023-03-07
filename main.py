a8ed06bf99cdee19e1f7d670630d4705fdcd9820
EdwardSJ151 <edwardsj13579@gmail.com>
2023-03-07 15:18:24 -0300

    github_compatibility

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('ola...')
+from subprocess import call
+
+
+def run_webapp_main():
+    call(["python", "pkdx_website_complete.py"])
+
+
+def run_webapp_study():
+    call(["python", "pkdx_study_website_complete.py"])
+
+
+run_webapp_main()
